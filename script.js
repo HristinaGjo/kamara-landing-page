7b0c7da841e6fb6c68a2b77aa157c33b23f0be89
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-06-20 14:00:46 +0200

    starting over, initial slide in index

--- script.js
+++ script.js
@@ -1,165 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-
+document.addEventListener("DOMContentLoaded", function() {
     const sliderContent = [
-        {
-            title: "Echoes",
-            images: [
-                "./assets/echoes-1.webp",
-                "./assets/echoes-2.webp"
-            ]
-        },
-        {
-            title: "Odyssey",
-            images: [
-                "./assets/odyssey-1.webp",
-                "./assets/odyssey-2.webp"
-            ]
-        },
-        {
-            title: "Cracker",
-            images: [
-                "./assets/cracker-1.webp",
-                "./assets/cracker-2.webp"
-            ]
-        },
-        {
-            title: "Dystopia",
-            images: [
-                "./assets/wasteland-1.webp",
-                "./assets/wasteland-2.webp"
-            ]
-        }
+        "Echoes",
+        "Odyssey",
+        "Cracker",
+        "Dystopia"
     ];
 
-
+    const slider = document.querySelector(".slider");
     let activeSlide = 0;
+    const imagePath = "./assets/"; // Base path for your images
 
-    function createNewSlide(index) {
-        const slideTheme = index % 2 ? "light" : "dark";
-        const slideData = sliderContent[index];
-
-        const newSlide = document.createElement("div");
-        newSlide.classList.add("slide");
-        newSlide.classList.add(slideTheme);
-        newSlide.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
-
-        const newSlideImg1 = document.createElement("div");
-        newSlideImg1.className = "slide-img slide-img-1";
-        const img1 = document.createElement("img");
-        img1.src = slideData.images[0];
-        img1.style.top = "100%";
-        newSlideImg1.appendChild(img1);
-        newSlide.appendChild(newSlideImg1);
-
-        const newSlideContent = document.createElement("div");
-        newSlideContent.classList.add("slide-content");
-        newSlideContent.innerHTML = `<h1>${slideData.title}</h1>`;
-        newSlide.appendChild(newSlideContent);
-
-        const newSlideImg2 = document.createElement("div");
-        newSlideImg2.className = "slide-img slide-img-2";
-        const img2 = document.createElement("img");
-        img2.src = slideData.images[1];
-        img2.style.top = "100%";
-        newSlideImg2.appendChild(img2);
-        newSlide.appendChild(newSlideImg2);
-
-        // Add pagination element
-        const pagination = document.createElement("div");
-        pagination.classList.add("pagination");
-        pagination.innerHTML = `
-            <span class="current-slide">${index + 1}</span>
-            <div class="line"><hr/></div>
-            <span class="total-slides">${sliderContent.length}</span>
-        `;
-        newSlide.appendChild(pagination);
-
-         // Add description element
-         const description = document.createElement("div");
-         description.classList.add("description");
-         description.innerHTML = `
-             <span>[photographer]</span>
-         `;
-         newSlide.appendChild(description);
-
-        return newSlide;
-    }
-
-    function initializeSlider() {
-        const mainContent = document.getElementById("main-content");
-
-        const initialSlide = createNewSlide(activeSlide);
-        mainContent.appendChild(initialSlide);
-
-        const img1 = initialSlide.querySelector(".slide-img-1 img");
-        const img2 = initialSlide.querySelector(".slide-img-2 img");
-
-        gsap.to(initialSlide, {
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-            duration: 1.5,
-            ease: "power4.inOut",
-            onStart: () => {
-                gsap.to([img1, img2], {
-                    top: "50%",
-                    duration: 1.5,
-                    ease: "power4.inOut"
-                });
-            }
-        });
-
-        gsap.fromTo(initialSlide.querySelector("h1"), {
-            scale: 1.5
-        }, {
-            scale: 1,
-            duration: 1.5,
-            ease: "power4.inOut"
-        });
-    }
-
-    function showMainContent() {
-        const preloader = document.getElementById("preloader");
-        const mainContent = document.getElementById("main-content");
-
-        // Animate out the preloader
-        gsap.to(preloader, {
-            opacity: 0,
-            duration: 1,
-            onComplete: () => {
-                preloader.style.display = "none";
-                gsap.to(mainContent, {
-                    opacity: 1,
-                    duration: 0.5,
-                    onStart: () => {
-                        mainContent.classList.remove("hidden");
-                    },
-                    onComplete: initializeSlider // Initialize slider after main content is fully visible
-                });
-            }
-        });
-    }
-
-    function simulateLoading() {
-        let percentage = 0;
-        const loadingText = document.getElementById("loading-text");
-
-        const loadingInterval = setInterval(() => {
-            percentage += 1;
-            loadingText.textContent = `${percentage}%`;
-
-            if (percentage >= 100) {
-                clearInterval(loadingInterval);
-                showMainContent();
-            }
-        }, 10); // Adjust this value for faster or slower loading simulation
-    }
-
-    simulateLoading();
-
-    // Slider functionality
-    document.addEventListener("click", function () {
-        const mainContent = document.getElementById("main-content");
-        const currentSlide = mainContent.querySelector(".slide:not(.exiting)");
+    document.addEventListener("click", function() {
+        const currentSlide = slider.querySelector(".slide:not(.exiting)");
+        const slideTheme = activeSlide % 2 ? "dark" : "light";
         activeSlide = (activeSlide + 1) % sliderContent.length;
 
         if (currentSlide) {
@@ -172,10 +25,36 @@ document.addEventListener("DOMContentLoaded", function () {
             currentSlide.classList.add("exiting");
         }
 
-        const newSlide = createNewSlide(activeSlide);
-        mainContent.appendChild(newSlide);
+        const newSlide = document.createElement("div");
+        newSlide.classList.add("slide");
+        newSlide.classList.add(slideTheme);
+        newSlide.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
 
-        const [img1, img2] = newSlide.querySelectorAll("img");
+        const newSlideImg1 = document.createElement("div");
+        newSlideImg1.className = "slide-img slide-img-1";
+        const img1 = document.createElement("img");
+        img1.src = `${imagePath}slider-${activeSlide + 1}-1.webp`; // Example: ./assets/slider-1-1.jpg
+        img1.style.top = "100%";
+        newSlideImg1.appendChild(img1);
+        newSlide.appendChild(newSlideImg1);
+
+        const newSlideContent = document.createElement("div");
+        newSlideContent.classList.add("slide-content");
+        newSlideContent.innerHTML = `<h1>${sliderContent[activeSlide]}</h1>`;
+        newSlide.appendChild(newSlideContent);
+
+        const newSlideImg2 = document.createElement("div");
+        newSlideImg2.className = "slide-img slide-img-2";
+        const img2 = document.createElement("img");
+        img2.src = `${imagePath}slider-${activeSlide + 1}-2.webp`; // Example: ./assets/slider-1-2.jpg
+        img2.style.top = "100%";
+        newSlideImg2.appendChild(img2);
+        newSlide.appendChild(newSlideImg2);
+
+        slider.appendChild(newSlide);
+
+
+
         gsap.to(newSlide, {
             clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
             duration: 1.5,
@@ -187,16 +66,21 @@ document.addEventListener("DOMContentLoaded", function () {
                     ease: "power4.inOut"
                 });
             },
-
+            onComplete: () => {
+                removeExtraSlides(slider);
+            }
         });
 
-        gsap.fromTo(newSlide.querySelector("h1"), {
-            scale: 1.5
-        }, {
+        gsap.to(".slide-content h1", {
             scale: 1,
             duration: 1.5,
             ease: "power4.inOut"
         });
     });
 
+    function removeExtraSlides(container) {
+        while (container.children.length > 5) {
+            container.removeChild(container.firstChild);
+        }
+    }
 });
